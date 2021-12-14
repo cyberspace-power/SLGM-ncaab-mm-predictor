bd2baa027766d6d5b59c025357d76705b5396528
cyberspace-power <gjs3qd@virginia.edu>
2021-12-14 16:36:00 -0500

    First commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import os
+
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
